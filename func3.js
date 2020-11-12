5760c8ef246fe341c8212206d8aa9249f9441228
InjaehugoLee <lij5109@gmail.com>
2020-11-12 13:13:02 +0900

    commit

--- func3.js
+++ func3.js
@@ -1,6 +1,7 @@
 var o = {
   k1:'v1',
-  k2:'v2'
+  k2:'v2',
+  k3:'v3'
 }
 function f1(){
   console.log(o.k1)
@@ -8,5 +9,9 @@ function f1(){
 function f2(){
   console.log(o.k2)
 };
+function f3(){
+  console.log(o.k3)
+};
 f1();
 f2();
+f3();
